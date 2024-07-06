b719f7a9e99058997a7aee04b0e133d53f4b6efc
Richard1019 <richardviktorik@gmail.com>
2024-07-06 15:17:47 +0200

    greeting message changed

--- Hangman 1.0/Program.cs
+++ Hangman 1.0/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            Console.WriteLine("Let us get started with the Hangman project");
+           
         }
     }
 }
