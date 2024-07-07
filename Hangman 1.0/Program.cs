0a430e4fe4226caaa2ea9feb608eaf6ace076932
Richard1019 <richardviktorik@gmail.com>
2024-07-07 14:57:30 +0200

    Generating a list of words to choosee from

--- Hangman 1.0/Program.cs
+++ Hangman 1.0/Program.cs
@@ -6,14 +6,14 @@
         {
             const int HANGMAN_PIECES = 8;
 
-           // List<string> WordList = new List<string>();
+           List<string> WordList = new List<string>();
 
-           // WordList.Add("aircraft");
-            //WordList.Add("bootcamp");
-            //WordList.Add("pizzeria");
+           WordList.Add("aircraft");
+           WordList.Add("bootcamp");
+           WordList.Add("pizzeria");
 
-            //Console.WriteLine("Hello, let us play some Hangman.");
-            //Console.WriteLine("I`m gonna choose a word, you try to guess it by choosing letters");
+           Console.WriteLine("Hello, let us play some Hangman.");
+           Console.WriteLine("I`m gonna choose a word, you try to guess it by choosing letters");
             
 
 
