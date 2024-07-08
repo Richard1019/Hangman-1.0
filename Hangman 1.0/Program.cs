1b2ea753274c4c1eb3253a258d57220473ccc651
Richard1019 <richardviktorik@gmail.com>
2024-07-08 18:30:10 +0200

    Game begins with the first word to guess

--- Hangman 1.0/Program.cs
+++ Hangman 1.0/Program.cs
@@ -4,27 +4,43 @@
     {
         static void Main(string[] args)
         {
-           const int HANGMAN_PIECES = 8;
 
-           List<string> WordList = new List<string>();
+            const int HANGMAN_PIECES = 8;
 
-           WordList.Add("aircraft");
-           WordList.Add("bootcamp");
-           WordList.Add("pizzeria");
+            List<string> WordList = new List<string>();
 
-           Console.WriteLine("Hello, let us play some Hangman.");
-           Console.WriteLine("I`m gonna choose a word, you try to guess it by choosing letters");
-           // Console.WriteLine(WordList[1]);
+            WordList.Add("aircraft");
+            WordList.Add("bootcamp");
+            WordList.Add("pizzeria");
 
-           Console.WriteLine("Ok, I am ready! Go a head, guess those letters!");
+            Console.WriteLine("Hello, let us play some Hangman.");
+            Console.WriteLine("I`m gonna choose a word, you try to guess it by choosing letters");
+            // Console.WriteLine(WordList[1]);
+
+            Console.WriteLine("Ok, I am ready! Go a head, guess those letters!");
 
             string YourFirstLetter = Console.ReadLine();
 
+            List<string> FirstWord = new List<string>();
+
+            FirstWord.Add("a");
+            FirstWord.Add("i");
+            FirstWord.Add("r");
+            FirstWord.Add("c");
+            FirstWord.Add("r");
+            FirstWord.Add("a");
+            FirstWord.Add("f");
+            FirstWord.Add("t");
 
 
-
-
-           
+           // foreach (string item in FirstWord) 
+            //{
+                 //if (YourFirstLetter == item) 
+                // {
+                 //   Console.WriteLine("Nice try");
+                 //   Console.WriteLine();
+                // }
+            
         }
     }
 }
